b5fa470d89d0161b48e7975e710e0c3d84bf3615
Thorsten-Michael Deinert <thorsten.deinert@udo.edu>
2017-07-27 13:04:03 +0200

    Debug-Anweisungen entfernt und Regexp überarbeitet

--- lib/amazon_s3/patches/patch_pdf_attach_for_external_files.rb
+++ lib/amazon_s3/patches/patch_pdf_attach_for_external_files.rb
@@ -2,16 +2,10 @@ module AmazonS3
   module PatchPdfAttachForExternalFiles
     def attach(attachments, filename, encoding)
       attachment = super(attachments, filename, encoding)
-      p attachment
       return attachment if attachment
 
-      
-      puts filename
-
-      /attachment.*\/(\d+)\/*.*\.(gif|jpeg|jpg|png)/.match(filename) do |match|
-        p match
+      /attachments\/download\/(\d+)\/.*\.(gif|jpeg|jpg|png)\z/.match(filename) do |match|
         attachment = Attachment.find(match[1])
-        p attachment
         if attachment && attachment.readable? && attachment.visible?
           return attachment
         else
